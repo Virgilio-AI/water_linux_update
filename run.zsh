c609a5fbb1de98b1561258afcb7fa7dcbd93f62d
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-07-22 12:08:24 -0500

    added a run.zsh for the updates

---
+++ run.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/zsh
+###############################
+# Author: Virgilio Murillo Ochoa
+# Date: 22/July/2022 - Friday
+# personal github: Virgilio-AI
+# linkedin: https://www.linkedin.com/in/virgilio-murillo-ochoa-b29b59203
+# contact: virgiliomurilloochoa1@gmail.com
+# web: virgiliomurillo.com
+# #########################################
+
+pacman -S python-pudb
+pacman -S rust
+pacman -S expect
+pacman -S unbuffer
+pacman -S net-tools
+cargo install leetcode-cli
+
